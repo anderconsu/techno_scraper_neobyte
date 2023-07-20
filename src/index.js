71449f03231fdd3ef828480f7314da1a3c6825ce
danel-bbk <dlafuente003@gmail.com>
2023-07-20 13:44:00 +0200

    scrapController

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import AmazonController from './controllers/amazonController.js';
+import ScrapController from './controllers/scrapController.js';
 const app = express();
 
 app.set("view engine", "pug");
@@ -9,20 +9,21 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    const amazonController = new AmazonController();
-    amazonController.getDataFromDB(req,res);
+    const scrapController = new ScrapController();
+    scrapController.getDataFromDB(req,res);
     });
 
 app.get('/scrap', (req, res) => {
     res.render('scrap');
     });
+    
 
 app.post('/scrap', async (req, res) => {
-    const amazonController = new AmazonController();
-    await amazonController.init();
+    const scrapController = new ScrapController();
+    await scrapController.init();
     let query  = req.body.query;
     let pages = 4;
-    const content = await amazonController.getData(query, pages);
+    const content = await scrapController.getData(query, pages);
     res.send(content);
     }
 );
